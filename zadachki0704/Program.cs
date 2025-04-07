5e559bf6e349296dee8832a8ed9f06c6ad660263
MEGA <MEGA@gmail.com>
2025-04-07 18:56:53 +0300

    Add project files.

---
+++ zadachki0704/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadachki0704
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] numbers = input.Split().Select(int.Parse).ToArray();
+
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int num in numbers)
+            {
+                seen.Add(num);
+            }
+
+            Console.WriteLine(string.Join(" ", seen));
+        }
+    }
+}
